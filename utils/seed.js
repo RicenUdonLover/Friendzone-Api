28897a4a5ddccafd84274a0bb3a4005b69ae632b
Lian Liu <riceudon@gmail.com>
2023-03-05 17:44:51 -0800

    Created seed.js using faker library

--- utils/seed.js
+++ utils/seed.js
@@ -1,30 +1,60 @@
-const connection = require('../config/connection');
-const { Post, User } = require('../models');
-const getRandomName = require('./data');
+const faker = require('faker');
+const { User, Thought } = require('../models');
 
-console.log(getRandomName());
-connection.on('error', (err) => err);
+const NUM_USERS = 10;
+const NUM_THOUGHTS = 50;
 
-connection.once('open', async () => {
-  console.log('connected');
-  await Post.deleteMany({});
-  await User.deleteMany({});
-
+// Generate some fake user data
+const generateUsers = (numUsers) => {
   const users = [];
-
-  for (let i = 0; i < 20; i++) {
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-
-    users.push({
-      first,
-      last,
-      age: Math.floor(Math.random() * (99 - 18 + 1) + 18),
+  for (let i = 0; i < numUsers; i++) {
+    const user = new User({
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
     });
+    users.push(user);
   }
+  return users;
+};
 
-  await User.collection.insertMany(users);
-  console.log(users);
-  process.exit(0);
-});
+// Generate some fake thought data
+const generateThoughts = (numThoughts, users) => {
+  const thoughts = [];
+  for (let i = 0; i < numThoughts; i++) {
+    const thought = new Thought({
+      thoughtText: faker.lorem.sentence(),
+      username: users[Math.floor(Math.random() * users.length)].username,
+    });
+    thoughts.push(thought);
+  }
+  return thoughts;
+};
+
+// Seed the database
+const seed = async () => {
+  try {
+    // Delete any existing data
+    await User.deleteMany({});
+    await Thought.deleteMany({});
+
+    // Generate fake data
+    const users = await User.create(generateUsers(NUM_USERS));
+    const thoughts = await Thought.create(generateThoughts(NUM_THOUGHTS, users));
+
+    // Add thoughts to users' thoughts array field
+    for (let i = 0; i < thoughts.length; i++) {
+      const user = users.find((u) => u.username === thoughts[i].username);
+      user.thoughts.push(thoughts[i]._id);
+      await user.save();
+    }
+
+    console.log(`Successfully seeded database with ${NUM_USERS} users and ${NUM_THOUGHTS} thoughts!`);
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+seed();
+
